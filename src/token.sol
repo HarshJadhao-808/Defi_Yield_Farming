2ab6eb5766f783bc33b7eb7624156fd4ba16a4b4
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2026-03-03 12:21:21 +0530

    Yield Farming

--- src/token.sol
+++ src/token.sol
@@ -1,3 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Hcoin is ERC20{
+    address owner;
+    mapping(address => bool) public Users;
+
+    constructor() ERC20("Hcoin","HC") {
+        owner = msg.sender;
+    }
+
+    function GiveHc (address userAddress) external {
+        require(Users[userAddress] == false );
+    }
+
+}
