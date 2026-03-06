19385211a387553547f9349a05f2f896121451b0
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2026-03-06 19:58:00 +0530

    DEX COMPLETED

--- src/DEX.sol
+++ src/DEX.sol
@@ -2,10 +2,11 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DEX is ERC20Burnable , ERC20{
+contract DEX is ERC20Burnable {
    uint256 public reserveHC;
    uint256 public reserveYT;
    address private owner;
+   
     IERC20 public HC;
     IERC20 public YT;
 
@@ -19,26 +20,51 @@ contract DEX is ERC20Burnable , ERC20{
         _mint(account,LP);
     }
 
-    function LPAddliquidity(uint256 HCamount ,uint256 YTamount) public {
-        require(HCamount*reserveYT == YTamount*reserveHC,"Ratio not Matched");
-        HC.transferFrom(msg.sender, address(this), HCamount);
-        YT.transferFrom(msg.sender, address(this), YTamount);
-        reserveHC += HCamount;
-        reserveYT += YTamount;
-        // amount = HCamount
-        giveLP(msg.sender,amount);
+    function SquareRoot (uint256 x) private pure returns(uint256) {
+         uint256 z = (x+1)/2;
+         uint256 y = x;
+
+         while(z<y){
+            y=z;
+            z=(x/z + z)/2;
+         }
+         return y;
     }
 
-    function AddLiquidity (uint256 HCamount,uint256 YTamount) external {
+    function AddLiquidity (uint256 HCamount,uint256 YTamount) public {
+        if(reserveHC ==0 && reserveYT == 0){
         require(msg.sender == owner,"Not Allowed");
             HC.transferFrom(msg.sender, address(this), HCamount);
             YT.transferFrom(msg.sender, address(this), YTamount);
+            uint256 liquidity = SquareRoot(HCamount*YTamount);
+            giveLP(owner, liquidity);
             reserveHC += HCamount;
             reserveYT += YTamount;
+        }else{
+        require(HCamount*reserveYT == YTamount*reserveHC,"Ratio not Matched");
+            HC.transferFrom(msg.sender, address(this), HCamount);
+            YT.transferFrom(msg.sender, address(this), YTamount);
+            uint256 liquidity = (HCamount*totalSupply()) / reserveHC;
+            reserveHC += HCamount;
+            reserveYT += YTamount;
+            giveLP(msg.sender,liquidity);
+        }
+    }
+    function removeLiquidity(uint256 LP) public {
+        require(LP>0,"0 LP cannot be buned");
+        require(balanceOf(msg.sender)>=LP,"0 LP cannot be buned");
+        uint256 amountHC = LP*reserveHC/totalSupply();
+        uint256 amountYT = LP*reserveYT/totalSupply();
+        _burn(msg.sender, LP);
+        reserveHC -= amountHC;
+        reserveYT -= amountYT;
+        HC.transfer(msg.sender, amountHC);
+        YT.transfer(msg.sender, amountYT);
     }
 
     function getAmount (uint256 amountIn,uint256 reserveIn,uint256 reserveOut) internal pure returns(uint256){
-        uint256 amountOut= (amountIn * reserveOut)/(reserveIn + amountIn);
+        uint256 feesRemoved = amountIn-(5*amountIn/1000);
+        uint256 amountOut= (feesRemoved * reserveOut)/(reserveIn + feesRemoved);
         return amountOut;
     }
 
@@ -48,7 +74,7 @@ contract DEX is ERC20Burnable , ERC20{
         YT.transferFrom(msg.sender,address(this),_YT);
         HC.transfer(msg.sender, amountOut);
         reserveYT += _YT;
-        reserveHC -=amountOut;
+        reserveHC -= amountOut;
     }
 
     function swapHC(uint256 _HC) public {
--- src/stake.sol
+++ src/stake.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import "./token.sol";
+import "./Hcoin.sol";
 import "./Yield.sol";
 
 contract Stake {
